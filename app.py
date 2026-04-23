1277c8bf5b7daf8e4d05aefda92d002889d333d7
Sahana Patil <your-email@gmail.com>
2026-04-23 19:20:45 +0530

    project

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import streamlit as st
-import numpy as np
+import requests
 
 st.title("Dynamic Pricing Engine 💰")
 
 inventory = st.slider("Inventory", 10, 500, 100)
 quantity = st.slider("Quantity Sold", 1, 20, 5)
 
-prediction = inventory * 0.5 + quantity * 2
+if st.button("Predict Price"):
+    url = "https://dynamic-pricing-backend-rywt.onrender.com/predict"
+    params = {
+        "inventory": inventory,
+        "quantity": quantity
+    }
 
-st.write("### Predicted Price:", prediction)
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        result = response.json()
+        st.success(f"Predicted Price: ₹{result['predicted_price']}")
+    else:
+        st.error("Failed to get prediction from backend")
